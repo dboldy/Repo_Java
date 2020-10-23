a43242f577e7b37a9f11443b66d668381e004746
dboldy <70780505+dboldy@users.noreply.github.com>
2020-10-23 13:53:31 -0400

    code test remark

--- src/codeTest.java
+++ src/codeTest.java
@@ -3,13 +3,12 @@ public class codeTest {
         int[] numbers = {32, 14, 24, 98, 24, 56, 148, 24, 78, 24};
         int inputNum = 24;
         int[] newNumbers = removeNumbers(numbers, inputNum);
-
+        int userSpeed = 89;
+        checkSpeed(userSpeed);
         System.out.println("Array after removal contains: " );
         for (int x : newNumbers) {
             System.out.print( x + " ");
 
-            int userSpeed = 70;
-            checkSpeed(userSpeed);
         }
     }
     public static int[] removeNumbers(int[] inArray, int inNum) {
@@ -31,7 +30,7 @@ public class codeTest {
         return numberRemoved;
     }
     public static void checkSpeed (int inSpeed) {
-        int points = (inSpeed - 70);
+       int points = (inSpeed - 70);
         if (points < 1) {
             System.out.println("user speed = " + inSpeed + " Thank you for driving with speed limit");
         }else if (points > 0 && points <5){
